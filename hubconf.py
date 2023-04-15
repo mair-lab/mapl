0d350104a4c09827b13c445d6f91288fb94834ef
Oscar Manas <oscar.manas@mila.quebec>
2023-04-15 09:30:47 -0400

    Fix typo

--- hubconf.py
+++ hubconf.py
@@ -8,5 +8,5 @@ from mapl import MAPL
 
 
 def mapl(**kwargs: Any) -> nn.Module:
-    checkpoint_url = 'https://github.com/mair-lab/mapl-private/raw/main/checkpoints/mapl-clip-vit-l14-gpt-j-coco.pt'
+    checkpoint_url = 'https://github.com/mair-lab/mapl/raw/main/checkpoints/mapl-clip-vit-l14-gpt-j-coco.pt'
     return MAPL.from_pretrained(checkpoint_url, **kwargs)
